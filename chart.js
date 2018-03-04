dfb206122381df2a038853df1cfb4cbc2206bf80
lkkchung <lkkchung@gmail.com>
2018-03-03 19:23:48 -0500

    added chart function

--- chart.js
+++ chart.js
@@ -1,3 +1,15 @@
 function chart(){
-    
+    createCanvas(2000, 2000);
+    background(255);
+
+    translate(270, 400);
+    for (let i = 0; i < fastDates.length; i++) {
+        stroke(0);
+        strokeWeight(10);
+        line(i * 50, 1440);
+
+        stroke(255, 20, 20);
+        strokeWeight(15);
+        line(sunrises.value[i], sunsets.value[i]);
+    }
 }
